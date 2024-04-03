a60348875a4569463e9d151017ac654353230557
Xavier Alfeiran <xavieralfeiran@Xaviers-MacBook-Pro.local>
2024-04-03 09:37:42 -0500

    google tag

--- ppolicy_es_gen_view.php
+++ ppolicy_es_gen_view.php
@@ -14,15 +14,13 @@
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/css/bootstrap-theme.css" rel="stylesheet" />
     <link rel="icon" type="image/png" href="assets/images/favicon.png"/>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VPJJCHEV8F"></script>
     <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-      ga('create', 'UA-80040509-1', 'auto');
-      ga('send', 'pageview');
-
+      gtag('config', 'G-VPJJCHEV8F');
     </script>
     <style>
       .jumbotron {
