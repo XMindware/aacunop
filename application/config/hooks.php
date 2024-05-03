9e955a10a07bb52883ffcc12a98b357b8ab5bbcb
Xavier Alfeiran <xavieralfeiran@Xaviers-MacBook-Pro.local>
2024-05-03 13:20:42 -0500

    hooks getenv

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	http://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_system'] = function() {
+	$dotenv = Dotenv\Dotenv::create(APPPATH);
+	$dotenv->load();
+};
